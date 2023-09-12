2d13f8da09b18dc869e6861955c254b7c212b485
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2023-09-12 20:52:55 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,23 @@
+!rm -r ~/.kaggle
+!mkdir ~/.kaggle
+!mv ./kaggle.json ~/.kaggle/
+!chmod 600 ~/.kaggle/kaggle.json
+
+!kaggle datasets download -d wobotintelligence/face-mask-detection-dataset
+!kaggle datasets download -c wobotintelligence/face-mask-detection-dataset
+
+import zipfile
+zip_ref = zipfile.ZipFile('face-mask-detection-dataset.zip', 'r')
+zip_ref.extractall('/content')
+zip_ref.close()
+
+
+from google.colab import drive
+drive.mount('/content/drive'
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+%matplotlib inline
+
