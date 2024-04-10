9a9cab197f8fa98a225f4366eecfc1ae3b87e545
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2024-04-10 20:09:22 +0530

    Delete ML_Algos directory

--- ML_Algos/base_estimator.py
+++
@@ -1,5 +0,0 @@
-sklearn.base: Base classes and utility functions
-===================================================
-# https://scikit-learn.org/stable/modules/classes.html#module-sklearn.base
-class BaseEstimator:
-    """Base class for all estimators in scikit-learn.
