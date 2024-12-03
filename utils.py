74f04ef0e461fcfabb34e0f8b0c87f6b9a7c8f9f
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2024-12-03 14:26:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,3 +41,22 @@ os.environ["HF_TOKEN"] = userdata.get('HF_TOKEN')
 ollama serve &  ollama pull llama --> start service
 !ollama list
 !ollama pull llama
+
+
+
+# # Start the ZooKeeper service
+# bin/zookeeper-server-start.sh config/zookeeper.properties
+# # Start the Kafka broker service
+# bin/kafka-server-start.sh config/server.properties
+# # create topic to store events
+# bin/kafka-topics.sh --create --topic testkafka-events --bootstrap-server localhost:9092
+# bin/kafka-topics.sh --describe --topic testkafka-events --bootstrap-server localhost:9092
+# # producer
+# bin/kafka-console-producer.sh --topic testkafka-events --bootstrap-server localhost:9092
+# # consumer
+# bin/kafka-console-consumer.sh --topic testkafka-events --from-beginning --bootstrap-server localhost:9092
+# # delete any data of your local Kafka environment including any events
+# rm -rf /tmp/kafka-logs /tmp/zookeeper /tmp/kraft-combined-logs
+# # alternatively to create topic
+# kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic monthly-report
+
